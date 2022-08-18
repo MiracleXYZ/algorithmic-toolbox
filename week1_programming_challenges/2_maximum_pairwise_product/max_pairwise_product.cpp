e4041a80f346933dab2c798e5bb88fd19d399b50
MiracleXYZ <xieyuzhang_xyz@foxmail.com>
2022-08-18 22:26:15 +0800

    Just find the 2 max numbers

--- week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
+++ week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
@@ -16,6 +16,26 @@ long long MaxPairwiseProduct(const std::vector<int>& numbers) {
     return max_product;
 }
 
+long long MaxPairwiseProductFast(const std::vector<int>& numbers) {
+    int n = numbers.size();
+    
+    int max_index1 = -1;
+    for (int i = 0; i < n; ++i) {
+        if ((max_index1 == -1) || (numbers[i] > numbers[max_index1])) {
+            max_index1 = i;
+        }
+    }
+
+    int max_index2 = -1;
+    for (int j = 0; j < n; ++j) {
+        if ((numbers[j] != numbers[max_index1]) && ((max_index2 == -1) || (numbers[j] > numbers[max_index2]))) {
+            max_index2 = j;
+        }
+    }
+
+    return ((long long)(numbers[max_index1])) * numbers[max_index2];
+}
+
 int main() {
     int n;
     std::cin >> n;
@@ -24,6 +44,6 @@ int main() {
         std::cin >> numbers[i];
     }
 
-    std::cout << MaxPairwiseProduct(numbers) << "\n";
+    std::cout << MaxPairwiseProductFast(numbers) << "\n";
     return 0;
 }
